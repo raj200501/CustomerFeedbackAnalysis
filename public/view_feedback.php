6a7277a364fb6d7be57ecc1a4a9b880ad9ff8fd5
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-28 01:21:44 -0700

    Create view_feedback.php

---
+++ public/view_feedback.php
@@ -0,0 +1,33 @@
+<?php
+include_once '../config/config.php';
+include_once '../src/Controller/FeedbackController.php';
+
+$feedbackController = new FeedbackController($conn);
+$feedbacks = $feedbackController->getAllFeedback();
+
+include '../views/header.php';
+?>
+<h1>View Feedback</h1>
+<table>
+    <thead>
+        <tr>
+            <th>Customer</th>
+            <th>Feedback</th>
+            <th>Rating</th>
+            <th>Type</th>
+            <th>Submitted At</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($feedbacks as $feedback): ?>
+        <tr>
+            <td><?php echo htmlspecialchars($feedback['name']); ?></td>
+            <td><?php echo htmlspecialchars($feedback['feedback_text']); ?></td>
+            <td><?php echo htmlspecialchars($feedback['rating']); ?></td>
+            <td><?php echo htmlspecialchars($feedback['feedback_type']); ?></td>
+            <td><?php echo htmlspecialchars($feedback['created_at']); ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<?php include '../views/footer.php'; ?>
