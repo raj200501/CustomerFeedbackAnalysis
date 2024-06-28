67d99cec6c3ded9f816b1fcc58fff4017c380ded
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-28 01:27:05 -0700

    Create manage_feedback.php

---
+++ views/admin/manage_feedback.php
@@ -0,0 +1,30 @@
+<?php include '../header.php'; ?>
+<h1>Manage Feedback</h1>
+<table>
+    <thead>
+        <tr>
+            <th>Customer</th>
+            <th>Feedback</th>
+            <th>Rating</th>
+            <th>Type</th>
+            <th>Submitted At</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($feedbacks as $feedback): ?>
+        <tr>
+            <td><?php echo htmlspecialchars($feedback['name']); ?></td>
+            <td><?php echo htmlspecialchars($feedback['feedback_text']); ?></td>
+            <td><?php echo htmlspecialchars($feedback['rating']); ?></td>
+            <td><?php echo htmlspecialchars($feedback['feedback_type']); ?></td>
+            <td><?php echo htmlspecialchars($feedback['created_at']); ?></td>
+            <td>
+                <a href="respond.php?feedback_id=<?php echo $feedback['id']; ?>">Respond</a>
+                <a href="delete_feedback.php?feedback_id=<?php echo $feedback['id']; ?>">Delete</a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<?php include '../footer.php'; ?>
